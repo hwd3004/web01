f3643fb47803d4bbc6b180db5dffc9a206104aad
hwd3004 <hwd3004@gmail.com>
2021-12-31 21:17:07 +0900

    Update UserTest.java

--- src/test/java/com/cos/web01/UserTest.java
+++ src/test/java/com/cos/web01/UserTest.java
@@ -12,23 +12,23 @@ import com.cos.web01.domain.user.Users;
 @SpringBootTest
 public class UserTest {
 
-	@Autowired
-	UserRepository userRepository;
+	// @Autowired
+	// UserRepository userRepository;
 
-	@Test
-	public void test100_유저추가() {
-		userRepository.save(Users.builder().userId("테스트아이디").password("test").userName("테스트네임").build());
+	// @Test
+	// public void test100_유저추가() {
+	// userRepository.save(Users.builder().userId("테스트아이디").password("test").userName("테스트네임").build());
 
-		List<Users> userList = userRepository.findAll();
+	// List<Users> userList = userRepository.findAll();
 
-		System.out.println(userList.toString());
+	// System.out.println(userList.toString());
 
-//		User user = userList.get(0);
+	// // User user = userList.get(0);
 
-		for (Users user : userList) {
-			System.out.println(user.toString());
-		}
+	// for (Users user : userList) {
+	// System.out.println(user.toString());
+	// }
 
-	}
+	// }
 
 }
